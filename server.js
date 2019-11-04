1440a56829253130618be1a368e99fcea72bb79e
OlgaMart112 <opam112@ukr.net>
2019-11-05 00:27:28 +0200

    [app] commit

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const app = express();
 
 app.use(express.static(__dirname + '/dist/learn-angular-change-detaction'));
 
-app.get('/*', function (req) {
+app.get('/*', function (res) {
     res.sendFile(path.join(__dirname + '/dist/learn-angular-change-detaction/index.html'));
 });
 
