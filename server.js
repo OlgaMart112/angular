566da6679f9e15c788f0b0ddfe86c1668627bd05
OlgaMart112 <opam112@ukr.net>
2019-11-05 00:12:23 +0200

    [app] commit

--- server.js
+++ server.js
@@ -3,10 +3,10 @@ const path = require('path');
 
 const app = express();
 
-app.use(express.static(__dirname + '/angularproject'));
+app.use(express.static(__dirname + '/dist/learn-angular-change-detaction'));
 
-app.get('/*', (req, res) => {
-    res.sendFile(path.join(__dirname + '/angularapp/index.html'));
+app.get('/*', function (req) {
+    res.sendFile(path.join(__dirname + '/dist/learn-angular-change-detaction/index.html'));
 });
 
 app.listen(process.env.PORT || 8080);
