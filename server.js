889e478849d73daa249eb00845c0a84fdf3acb01
OlgaMart112 <44723124+OlgaMart112@users.noreply.github.com>
2019-11-05 15:01:14 +0300

    fix
    fix

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const path = require('path');
 
 const app = express();
 
-app.use(express.static(__dirname + '/learn-angular-change-detaction'));
+app.use(express.static(__dirname + '/dist/learn-angular-change-detaction'));
 
 app.get('/*', function (res) {
     res.sendFile(path.join(__dirname + '/dist/learn-angular-change-detaction/index.html'));
